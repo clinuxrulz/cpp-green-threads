19b6e935c8abec9dff97c375762831a8ed8524c9
Clinton Selke <clinuxrulz@gmail.com>
2016-02-26 18:31:58 +1000

    simplified lambda types

--- ContT.hpp
+++ ContT.hpp
@@ -16,24 +16,24 @@ M<R> runContT(ContT<R,M,A> c, F<A,M<R>> k) {
 template <typename R, template <typename> class M, typename A, typename B>
 ContT<R,M,B> map(const F<A,B>& f, const ContT<R,M,A>& ma) {
   ContT<R,M,B> r;
-  r.unContT = arr(std::function<M<R>(F<B,M<R>>)>([=](F<B,M<R>> k) {
-    return call(ma.unContT,arr(std::function<M<R>(A)>([=](A a) {
+  r.unContT = arr([=](F<B,M<R>> k) {
+    return call(ma.unContT,arr([=](A a) {
       return call(k, call(f, a));
-    })));
-  }));
+    }));
+  });
   return r;
 }
 
 template <typename R, template <typename> class M, typename A, typename B>
 ContT<R,M,B> apply(const ContT<R,M,F<A,B>>& mf, const ContT<R,M,A>& ma) {
   ContT<R,M,B> r;
-  r.unContT = arr(std::function<M<R>(F<B,M<R>>)>([=](F<B,M<R>> k) {
-    return call(mf.unContT,arr(std::function<M<R>(F<A,B>)>([=](F<A,B> f) {
-      return call(ma.unContT,arr(std::function<M<R>(A)>([=](A a) {
+  r.unContT = arr([=](F<B,M<R>> k) {
+    return call(mf.unContT,arr([=](F<A,B> f) {
+      return call(ma.unContT,arr([=](A a) {
         return call(k, call(f, a));
-      })));
-    })));
-  }));
+      }));
+    }));
+  });
   return r;
 }
 
@@ -49,11 +49,11 @@ ContT<R,M,A> pure(A a) {
 template <typename R, template <typename> class M, typename A, typename B>
 ContT<R,M,B> bind(const ContT<R,M,A>& ma, const F<A,ContT<R,M,B>>& f) {
   ContT<R,M,B> r;
-  r.unContT = arr(std::function<M<R>(F<B,M<R>>)>([=](F<B,M<R>> k) {
-    return call(ma.unContT,arr(std::function<M<R>(A)>([=](A a) {
+  r.unContT = arr([=](F<B,M<R>> k) {
+    return call(ma.unContT,arr([=](A a) {
       return call(call(f, a).unContT, k);
-    })));
-  }));
+    }));
+  });
   return r;
 }
 
